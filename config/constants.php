b3789cfcbbda0e99b47af21ee97eaaafbc238a37
vim <9wimu9@gmail.com>
2022-05-29 22:28:46 +0530

    config file for constants

---
+++ config/constants.php
@@ -0,0 +1,18 @@
+<?php
+//0->not approved,1->approved,2->approval pending
+return [
+    "question_approving" => [
+        "not_approved" => 0,
+        "approved" => 1,
+        "approval_pending" => 2,
+    ],
+    "paragraph_approving" => [
+        "not_approved" => 0,
+        "approved" => 1,
+        "approval_pending" => 2,
+    ],
+    "questions_from_paragraph" => [
+        "more_to_ask" => 0,
+        "no_more_to_ask" => 1,
+    ]
+];
